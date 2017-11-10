b2f137d7d075c968a3a5b3a21f430307edd3d271
Mike Troutman <mktrout09@gmail.com>
2017-11-09 19:20:56 -0500

    fixed sequelize err

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
 var apiRoutes = require('./app/routing/apiRoutes.js');
 var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
@@ -21,7 +29,14 @@ app.use('/', htmlRoutes);
 
 
 
-app.listen(PORT, function() {
+
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
+  });
 });
-});
+
+
