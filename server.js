789fc2de2ff50e0f5a7168ea2b9214ad55700c39
Mike Troutman <mktrout09@gmail.com>
2017-11-09 19:12:23 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
-// Requiring our models for syncing
-var db = require("./models");
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-app.use(express.static("public"));
+var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
 
-// Routes
-// =============================================================
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
-require("./routes/html-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
 });
