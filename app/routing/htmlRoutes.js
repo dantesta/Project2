59b4fc4885dcf57734cdda8dcc99ffbee7661f6f
Mike Troutman <mktrout09@gmail.com>
2017-11-09 19:08:07 -0500

    changed html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,21 +1,18 @@
+var express = require("express");
 var path = require("path");
+var router = express.Router();
 
 
 
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    console.log(__dirname);
 
-module.exports = function(app) {
+});
 
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+router.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+});
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-};
+module.exports = router;
