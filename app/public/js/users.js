430037833feca9b3f2f7579c1bc24425428898f7
Mike Troutman <mktrout09@gmail.com>
2017-11-14 19:57:03 -0500

    updated users.js

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // Getting references to the name inout and author container, as well as the table body
+  
   var nameInput = $("#name");
   var userList = $("tbody");
   var userContainer = $(".user-container");
@@ -7,35 +7,40 @@ $(document).ready(function() {
   var userName = $("#username");
 
  
-  // Adding event listeners to the form to create a new object, and the button to delete
-  // an Author
-  $(document).on("submit", "#.user-container", handleAuthorFormSubmit);
+  
+  $(document).on("submit", "#signup", handleUserFormSubmit);
   
 
-  // Getting the intiial list of Authors
-  getUsers();
+  
+        // getUsers();
 
-  // A function to handle what happens when the form is submitted to create a new Author
+  
   function handleUserFormSubmit(event) {
     event.preventDefault();
-    // Don't do anything if the name fields hasn't been filled out
+    
     if (!nameInput.val().trim().trim()) {
       return;
     }
-    // Calling the upsertAuthor function and passing in the value of the name input
+    
     upsertUser({
       name: nameInput.val().trim(),
       email: emailInput.val().trim(),
       username: userName.val().trim()
 
     });
+
   }
 
-  // A function for creating an author. Calls getAuthors upon completion
+
   function upsertUser(userData) {
-    $.post("/api/", userData)
-      // .then(getUsers);
+    console.log(userData)
+    $.post("/api/newuser", userData)
+      .then(function(data) {
+          console.log(data);
+      });
   }
+
+});
 
  
  
@@ -53,22 +58,22 @@ $(document).ready(function() {
   // }
 
   // A function for rendering the list of authors to the page
-  function renderAuthorList(rows) {
-    authorList.children().not(":last").remove();
-    authorContainer.children(".alert").remove();
-    if (rows.length) {
-      console.log(rows);
-      authorList.prepend(rows);
-    }
-    else {
-      renderEmpty();
-    }
-  }
+  // function renderAuthorList(rows) {
+  //   authorList.children().not(":last").remove();
+  //   authorContainer.children(".alert").remove();
+  //   if (rows.length) {
+  //     console.log(rows);
+  //     authorList.prepend(rows);
+  //   }
+  //   else {
+  //     renderEmpty();
+  //   }
+  // }
 
-  // Function for handling what to render when there are no authors
-  function renderEmpty() {
-    var alertDiv = $("<div>");
-    alertDiv.addClass("alert alert-danger");
-    alertDiv.text("You must create an Author before you can create a Post.");
-    authorContainer.append(alertDiv);
-  }
+  // // Function for handling what to render when there are no authors
+  // function renderEmpty() {
+  //   var alertDiv = $("<div>");
+  //   alertDiv.addClass("alert alert-danger");
+  //   alertDiv.text("You must create an Author before you can create a Post.");
+  //   authorContainer.append(alertDiv);
+  // }
